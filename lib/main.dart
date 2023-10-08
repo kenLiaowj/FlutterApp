8aa21c1b5b5f46b28b2b479e55eded54e0594c19
廖伟健 <>
2023-10-08 23:26:53 +0800

    创建了一个ListView视图

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'model/post.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
@@ -10,20 +11,58 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Bruce'),
-          foregroundColor: Colors.black,
-          elevation: 0.0,
-        ),
-        body: const Center(
-          child: Text(
-            'Hello Flutter',
-            style: TextStyle(fontSize: 18.0, color: Colors.red),
-          ),
-        ),
-      ),
+      home: const Home(),
       theme: ThemeData(primarySwatch: Colors.red),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Container(
+      color: const Color.fromARGB(255, 237, 236, 236),
+      margin: const EdgeInsets.only(top: 8, left: 8, right: 8),
+      child: Column(
+        children: [
+          Image.network(posts[index].imageUrl),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            posts[index].title,
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            posts[index].author,
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.normal),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bruce'),
+        foregroundColor: Colors.black,
+        elevation: 0.0,
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (context, index) {
+          return _listItemBuilder(context, index);
+        },
+      ),
     );
   }
 }
---
+++ lib/model/post.dart
@@ -0,0 +1,61 @@
+class Post {
+  const Post(
+      {required this.author, required this.title, required this.imageUrl});
+
+  final String author;
+  final String title;
+  final String imageUrl;
+}
+
+final List<Post> posts = [
+  const Post(
+      author: "Author 1",
+      title: "Title 1",
+      imageUrl:
+          "https://img2.baidu.com/it/u=2257799194,2566047008&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800"),
+  const Post(
+      author: "Author 2",
+      title: "Title 2",
+      imageUrl:
+          "https://img2.baidu.com/it/u=2257799194,2566047008&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800"),
+  const Post(
+      author: "Author 3",
+      title: "Title 3",
+      imageUrl:
+          "https://img2.baidu.com/it/u=2257799194,2566047008&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800"),
+  const Post(
+      author: "Author 4",
+      title: "Title 4",
+      imageUrl:
+          "https://img2.baidu.com/it/u=2257799194,2566047008&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800"),
+  const Post(
+      author: "Author 5",
+      title: "Title 5",
+      imageUrl:
+          "https://img2.baidu.com/it/u=2257799194,2566047008&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800"),
+  const Post(
+      author: "Author 6",
+      title: "Title 6",
+      imageUrl:
+          "https://img2.baidu.com/it/u=2257799194,2566047008&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800"),
+  const Post(
+      author: "Author 7",
+      title: "Title 7",
+      imageUrl:
+          "https://img2.baidu.com/it/u=2257799194,2566047008&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800"),
+  const Post(
+      author: "Author 8",
+      title: "Title 8",
+      imageUrl:
+          "https://img2.baidu.com/it/u=2257799194,2566047008&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800"),
+  const Post(
+      author: "Author 9",
+      title: "Title 9",
+      imageUrl:
+          "https://img2.baidu.com/it/u=2257799194,2566047008&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800"),
+  const Post(
+      author: "Author 10",
+      title: "Title 10",
+      imageUrl:
+          "https://img2.baidu.com/it/u=2257799194,2566047008&fm=253&fmt=auto&app=120&f=JPEG?w=1200&h=800")
+];
